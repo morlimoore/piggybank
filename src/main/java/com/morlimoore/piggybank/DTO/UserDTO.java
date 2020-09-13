a1515dcd33019bb17009a7d1122aa86afff483de
morlimoore <cooliyk2@gmail.com>
2020-09-13 22:22:42 +0100

    Proper modification on UserDTO

---
+++ src/main/java/com/morlimoore/piggybank/DTO/UserDTO.java
@@ -0,0 +1,30 @@
+package com.morlimoore.piggybank.DTO;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+public class UserDTO {
+
+    private Long id;
+
+    @NotBlank(message="First name cannot be blank")
+    private String first_name;
+
+    @NotBlank(message="Last name cannot be blank")
+    private String last_name;
+
+    @Email(message="Please enter a valid Email address")
+    private String email;
+
+    @NotBlank(message="Please a password")
+    private String password;
+
+    @NotBlank(message="Please enter a date of birth")
+    private String date_of_birth;
+
+    @CreationTimestamp
+    private LocalDateTime created_at;
+}
